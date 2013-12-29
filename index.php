77f2929e19fd17cc73efc2ca54bf0269ebb9bf9d
Simon Waldherr <git@simon.waldherr.eu>
2013-12-29 19:10:17 +0100

    cascading bugfix osf export bugfix minor fixes

--- index.php
+++ index.php
@@ -6,10 +6,24 @@
 
   $data['tags']         = '';
   $data['fullmode']     = 'true';
-  $data['mainmode']     = 'osf';
+  $data['mainmode']     = 'block';
 
   $osf_shownotes = file_get_contents('./example.osf.txt');
-  //$parsed = parserWrapperAPI(array('pad'=> html_entity_decode(base64_encode($osf_shownotes)), 'mainmode'=> $data['mainmode'], 'tags'=> $data['tags']));
+
   $parsed = parserWrapper($osf_shownotes);
+  $parsed2 = parserWrapper($parsed['osf']);
+  if ($parsed['osf'] != $parsed2['osf']) {
+    $parsed = explode("\n", $parsed['osf']);
+    $parsed2 = explode("\n", $parsed2['osf']);
+    for ($i = 0; $i < count($parsed); $i++) {
+      if ($parsed[$i] != $parsed2[$i]) {
+        echo $i.': '.$parsed[$i].' != '.$parsed2[$i]."\n";
+      }
+    }
+    $parsed = implode("\n", $parsed);
+  }
+  var_dump($parsed);
+
+  $parsed = parserWrapperAPI(array('pad'=> html_entity_decode(base64_encode($osf_shownotes)), 'mainmode'=> $data['mainmode'], 'tags'=> $data['tags']));
   var_dump($parsed);
 ?>
