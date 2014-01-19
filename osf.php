7b8bfbfb85ab2052277907639729072778e87e99
Simon Waldherr <git@simon.waldherr.eu>
2014-01-19 20:09:57 +0100

    fixed warning

--- osf.php
+++ osf.php
@@ -959,17 +959,19 @@ function osf_export_osf($array, $full = false, $template = '', $filtertags = arr
 function osf_export_chapterlist($array) {
   $returnstring = '';
   foreach ($array as $item) {
-    if ($item['chapter']) {
-      $filterpattern = array(
-        '((#)(\S*))',
-        '(\<((http(|s)://\S{0,128})>))',
-        '(\s+((http(|s)://\S{0,128})\s))'
-      );
-      $text = preg_replace($filterpattern, '', $item['orig']);
-      if (strpos($item['time'], '.')) {
-        $returnstring .= $item['time'] . ' ' . $text . "\n";
-      } else {
-        $returnstring .= $item['time'] . '.000 ' . $text . "\n";
+    if (isset($item['chapter'])) {
+      if ($item['chapter']) {
+        $filterpattern = array(
+          '((#)(\S*))',
+          '(\<((http(|s)://\S{0,128})>))',
+          '(\s+((http(|s)://\S{0,128})\s))'
+        );
+        $text = preg_replace($filterpattern, '', $item['orig']);
+        if (strpos($item['time'], '.')) {
+          $returnstring .= $item['time'] . ' ' . $text . "\n";
+        } else {
+          $returnstring .= $item['time'] . '.000 ' . $text . "\n";
+        }
       }
     }
   }
